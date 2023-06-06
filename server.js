7710f90196fdd5ddf580f8e161f39200637cefb4
fahsal marraz <fahsalmarraz@gmail.com>
2023-06-07 01:36:14 +0700

    crud sederhana

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get("/", function (req, res) {
   res.json({ status: "Jolly! ready to roll" });
 });
 
+/*--Memunculkan data dari database sesuai dengan nama yang di minta--*/
 app.get("/:bucket_list", async (req, res) => {
   const query = "SELECT * FROM lists WHERE name = ?";
   pool.query(query, [req.params.bucket_list], (error, results) => {
@@ -31,6 +32,7 @@ app.get("/:bucket_list", async (req, res) => {
   });
 });
 
+/*--Menambah data kedalam database--*/
 app.post("/", async (req, res) => {
   const data = {
     name: req.body.name,
@@ -42,6 +44,49 @@ app.post("/", async (req, res) => {
       res.json({ status: "failure", reason: error.code });
     } else {
       res.json({ status: "success", data: data });
+    }
+  });
+});
+
+/*--Mengubah data kedalam database--*/
+app.put("/:bucket_list", async (req, res) => {
+  const data = {
+    name: req.body.name,
+    amount: req.body.amount,
+  };
+  const query = "UPDATE lists SET name = ?, amount = ? WHERE name = ?";
+  pool.query(
+    query,
+    [data.name, data.amount, req.params.bucket_list],
+    (error, results) => {
+      if (error) {
+        console.error("Error executing query:", error);
+        res
+          .status(500)
+          .json({ status: "error", message: "Error executing query" });
+        return;
+      }
+
+      if (results.affectedRows === 0) {
+        res.json({ status: "failure", message: "Data not found" });
+      } else {
+        res.json({ status: "success", data: data });
+      }
+    }
+  );
+});
+
+/*--Menghapus data dari dalam database--*/
+app.delete("/:bucket_list", async (req, res) => {
+  const query = "DELETE FROM lists WHERE name = ?";
+  pool.query(query, [req.params.bucket_list], (error) => {
+    if (error) {
+      console.error("Error executing query:", error);
+      res
+        .status(500)
+        .json({ status: "error", message: "Error executing query" });
+    } else {
+      res.json({ status: "success" });
     }
   });
 });
