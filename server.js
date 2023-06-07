ab7a069864dc6bd8b23b2d1c2a63c5e4c93ddc04
fahsal marraz <fahsalmarraz@gmail.com>
2023-06-08 02:50:53 +0700

    update

--- server.js
+++ server.js
@@ -97,3 +97,27 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
 });
+
+/*--- CLOUD RUN TRIGGER PUB/SUB---*/
+app.post("/", (req, res) => {
+  if (!req.body) {
+    const msg = "no Pub/Sub message received";
+    console.error(`error: ${msg}`);
+    res.status(400).send(`Bad Request: ${msg}`);
+    return;
+  }
+  if (!req.body.message) {
+    const msg = "invalid Pub/Sub message format";
+    console.error(`error: ${msg}`);
+    res.status(400).send(`Bad Request: ${msg}`);
+    return;
+  }
+
+  const pubSubMessage = req.body.message;
+  const name = pubSubMessage.data
+    ? Buffer.from(pubSubMessage.data, "base64").toString().trim()
+    : "World";
+
+  console.log(`Hello ${name}!`);
+  res.status(204).send();
+});
