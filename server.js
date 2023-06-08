37dc02709ddd5d7f5bce5799345f503d8eaf9d5c
fahsal marraz <fahsalmarraz@gmail.com>
2023-06-08 23:22:20 +0700

    update

--- server.js
+++ server.js
@@ -121,3 +121,7 @@ app.post("/", (req, res) => {
   console.log(`Hello ${name}!`);
   res.status(204).send();
 });
+// [END run_pubsub_handler]
+// [END cloudrun_pubsub_handler]
+
+module.exports = app;
